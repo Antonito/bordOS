28c1d10d399803d34274a74e254f59afa417cca4
Lucas TRONCY <lucas.troncy@epitech.eu>
2017-01-29 00:25:40 +0100

    pulling header include

--- kernel/boot/kmain.c
+++ kernel/boot/kmain.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
-#include "multiboot.h"
+#include "kmain.h"
+#include "display/kernel_display.h"
 
 void halt()
 {
@@ -11,6 +12,9 @@ uint32_t kmain(t_boot_info *info, uint32_t eax)
 {
   if (eax != MBOOT_MAGIC2)
     return (1);
+  term_init();
+  set_term_color(vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK));
+  term_putstr("Hello, kernel!");
   for (;;)
     {
       halt();
